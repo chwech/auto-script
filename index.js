518a19eef2f95c2d5c0f7f1808c7b250057097c1
github-chwech <13672615289@163.com>
2020-05-31 16:53:14 +0800

    从打开支付宝到收集能量跑通

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 console.log('versionCode', app.versionCode)
 console.log('versionName', app.versionName)
 
+var log = console.log
+var tlog = toastLog
+
 // 检查无障碍服务是否已经启用，如果没有启用则跳转到无障碍服务启用界面，并等待无障碍服务启动；当无障碍服务启动后脚本会继续运行。
 auto.waitFor()
-
-sleep(3000)
 
 function launchAlipay () {
   return new Promise((resolve, reject) => {
@@ -19,19 +20,37 @@ function launchAlipay () {
   })
 }
 
+function waitFor(k) {
+  while (!descContains(k).exists()) {
+    sleep(1000)
+  }
+  return
+}
+
 launchAlipay()
   .then(() => {
-    console.log('启动成功')
-
+    log('启动成功')
     while (!click('蚂蚁森林'));
-    
-    sleep(3000)
 
-    id('h5_tv_title').findOne().parent().waitFor()
-    
-    className('android.widget.Button').untilFind().click()
+    waitFor('收集能量')
 
-    sleep(2000)
+    log('就绪')
 
-    desc('关闭').findOne().click()
+    var balls = className('Button')
+      .descContains('收集能量')
+      .find() // 不会阻塞
+
+    tlog('找到' + balls.length + '个能量球')
+
+    balls.forEach(item => {
+      // 基于控件的click点击收集不了，只能用基于坐标的click
+      var rect = item.bounds()
+      click(rect.centerX(), rect.centerY())
+    })
+
+    var isClose = desc('关闭').findOne().click()
+
+    if (isClose) {
+      log('关闭蚂蚁森林页面')
+    }
   })
